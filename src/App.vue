1f782c6e1e1c18ab893e2ce63d0bac5693fe7a7c
tony <852895752@qq.com>
2024-09-07 20:43:11 +0800

    daily_update

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
-<script setup>
+<script>
+import ToDoItem from "./components/ToDoItem.vue"
 
+export default {
+    name: "app",
+    components: {
+        ToDoItem,
+    }
+}
+
+    
 </script>
 
 <template>
     <div id="app">
-        <h1>To-Do List</h1>
+        <h1>My To-Do List</h1>
+        <ul>
+            <li>
+                <to-do-item label="My ToDo Item" :done="false"></to-do-item>
+            </li>
+        </ul>
     </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
 </style>
